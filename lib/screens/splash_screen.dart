b23d48cd5bf34bfa25467ab097a0752615bc3217
Bryan <hemantshrsth@gmail.com>
2021-10-28 13:29:15 +0530

    mark1 completed (all the credentail screens)

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:project_mark1/screens/credential_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateHome();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Image.asset('assets/images/splash_logo.png'),
+      )),
+    );
+  }
+
+  // Navigate to LoginScreen
+  void _navigateHome() async {
+    await Future.delayed(const Duration(seconds: 4), () {});
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (_) => const CredentialScreen()));
+  }
+}
